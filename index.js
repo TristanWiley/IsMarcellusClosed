06142a94c89e704e4c99201883ca6324c545cd26
Tristan Wiley <tristan@tristanwiley.com>
2016-11-25 18:13:38 -0500

    Fixed a lot

---
+++ index.js
@@ -0,0 +1,23 @@
+var isClosed = false;
+$.ajax({
+    url: 'http://ismarcellusclosed.com/Closings.html',
+    success: function(data) {
+        $(data).find(".orgname").each(function() {
+            var schoolName = this.innerText;
+            console.log(schoolName);
+            if (schoolName.indexOf('Marcellus') >= 0) {
+                isClosed = true;
+                $('.isclosed').text("YES!");
+            }
+            
+            if (schoolName.indexOf('Skaneateles') >= 0) {
+            	console.log("wat");
+            	$('.isskan').text("But Skan is like what the heck...");
+            }
+        });
+        if (isClosed != true) {
+            $('.isclosed').text("NOPE");
+            $('.sub').text("You have to go to school.")
+        }
+    }
+});
