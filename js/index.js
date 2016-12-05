83e41d78da5a9bb13f7719db32ddd71aa2aa6bb6
Tristan Wiley <tristan@tristanwiley.com>
2016-12-04 22:02:56 -0500

    Redy?

--- js/index.js
+++ js/index.js
@@ -1,27 +1,31 @@
 var isClosed = false;
-$.ajax({
-    url: 'closings.html',
-    success: function(data) {
-        $(data).find("tr").each(function() {
-            var schoolName = this.innerText;
-            console.log(schoolName);
-            if (schoolName.indexOf('Marcellus') >= 0) {
-                if (schoolName.includes("Delayed")) {
-                    $('.isclosed').text("Delayed!");                    
-                } else {
-                    isClosed = true;
-                    $('.isclosed').text("YES!");
+$(function() {
+    $.ajax({
+        url: 'closings.html',
+        success: function(data) {
+            $(data).find("tr").each(function() {
+                var schoolName = this.innerText;
+                console.log(schoolName);
+                if (schoolName.indexOf('Marcellus') >= 0) {
+                    if (schoolName.includes("Delayed")) {
+                        console.log("Delayed");
+                        $('.isclosed').text("Delayed!");
+                    } else {
+                        isClosed = true;
+                        console.log("Closed");
+                        $('.isclosed').text("YES!");
+                    }
                 }
-            }
 
-            if (schoolName.indexOf('Skaneateles') >= 0) {
-                console.log("wat");
-                $('.isskan').text("But Skan is like what the heck...");
+                if (schoolName.indexOf('Skaneateles') >= 0) {
+                    console.log("wat");
+                    $('.isskan').text("But Skan is like what the heck...");
+                }
+            });
+            if (isClosed != true) {
+                $('.isclosed').text("NOPE");
+                $('.sub').text("You have to go to school.")
             }
-        });
-        if (isClosed != true) {
-            $('.isclosed').text("NOPE");
-            $('.sub').text("You have to go to school.")
         }
-    }
+    });
 });
